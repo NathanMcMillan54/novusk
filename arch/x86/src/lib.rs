ef810e8357914d4822c8a8376e118fe968841eee
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-05-07 22:33:29 -0400

    Added lib.rs in arch/x86/src/ so Novusk can be added to an executable.

---
+++ arch/x86/src/lib.rs
@@ -0,0 +1,15 @@
+#![no_std]
+
+#![feature(global_asm, llvm_asm)]
+
+#[macro_use] extern crate uefi;
+#[macro_use] extern crate uefi_macros;
+#[macro_use] extern crate uefi_services;
+
+#[cfg(target_arch = "x86")]
+extern crate i686;
+
+pub mod boot;
+pub mod drivers;
+pub mod include;
+pub mod kernel;
--- arch/x86/src/main.rs
+++ arch/x86/src/main.rs
@@ -1,26 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(global_asm, llvm_asm)]
 
-#[macro_use] extern crate uefi;
-#[macro_use] extern crate uefi_macros;
-#[macro_use] extern crate uefi_services;
+extern crate x86_kernel;
 
-#[cfg(target_arch = "x86")]
-extern crate i686;
-
-pub mod boot;
-pub mod drivers;
-pub mod include;
-pub mod kernel;
-
-use core::panic::PanicInfo;
-
-
-/*
-There shouldn't be a panic handler because uefi-services already has one
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {  }
-}
-*/
