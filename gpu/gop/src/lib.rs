848fd5551d0f620d74ec1aa686989a446c943029
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-05-18 10:51:03 -0400

    Added GOP GPU driver.

---
+++ gpu/gop/src/lib.rs
@@ -0,0 +1,17 @@
+#![no_std]
+
+#[macro_use] extern crate kinfo;
+
+extern crate uefi;
+use uefi::proto::console::gop::GraphicsOutput;
+use uefi::prelude::BootServices;
+
+pub unsafe fn gop_init(bt: &BootServices) {
+    if let Ok(gop) = bt.locate_protocol::<GraphicsOutput>() {
+        let gop = gop.expect("Couldn't initialize GOP");
+        let gop = &mut *gop.get();
+        kinfo!("GOP init");
+    } else {
+        // TODO: Kerror
+    }
+}
