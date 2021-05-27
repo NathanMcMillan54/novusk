6a29ccd370d2cf5410b1186acbfa72fe35f8d2a4
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-05-27 17:24:17 -0400

    Worked on FAT fs.

--- fs/fat/src/lib.rs
+++ fs/fat/src/lib.rs
@@ -1,16 +1,74 @@
 #![no_std]
 
+#[macro_use] extern crate alloc;
+
 #[macro_use] extern crate kerror;
 #[macro_use] extern crate kinfo;
+#[macro_use] extern crate printk;
 
-use core::ptr::NonNull;
-use uefi::table::{Boot, SystemTable};
-
-extern "C" {
-    fn st() -> NonNull<SystemTable<Boot>>;
-}
+use libefi::st;
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::proto::media::file::{File, Directory};
 
 pub unsafe fn fat_init() {
     // This means BootServices and nothing else!
     let bs = st().as_ref().boot_services();
+
+    if let Ok(sfs) = bs.locate_protocol::<SimpleFileSystem>() {
+        let sfs = sfs.expect("Cannot open `SimpleFileSystem` protocol");
+        let sfs = unsafe { &mut *sfs.get() };
+        let mut root = sfs.open_volume().unwrap().unwrap();
+        let mut buffer = vec![0; 128];
+        loop {
+            let file_info = match root.read_entry(&mut buffer) {
+                Ok(completion) => {
+                    if let Some(info) = completion.unwrap() {
+                        info
+                    } else {
+                        break
+                    }
+                },
+                Err(error) => {
+                    let min_size = error.data().unwrap();
+                    buffer.resize(min_size, 0);
+                    continue;
+                },
+            };
+            printk!("Root directory:\n    {:?}", file_info);
+        }
+        root.reset_entry_readout().unwrap().unwrap();
+    } else {
+        kerror!("Couldn't initialize FAT Fs");
+    }
+}
+
+pub unsafe fn fat_reinit() -> Directory {
+    let bs = st().as_ref().boot_services();
+
+    if let Ok(sfs) = bs.locate_protocol::<SimpleFileSystem>() {
+        let sfs = sfs.expect("Cannot open `SimpleFileSystem` protocol");
+        let sfs = unsafe { &mut *sfs.get() };
+        let mut directory = sfs.open_volume().unwrap().unwrap();
+        let mut buffer = vec![0; 128];
+        loop {
+            let file_info = match directory.read_entry(&mut buffer) {
+                Ok(completion) => {
+                    if let Some(info) = completion.unwrap() {
+                        info
+                    } else {
+                        break
+                    }
+                },
+                Err(error) => {
+                    let min_size = error.data().unwrap();
+                    buffer.resize(min_size, 0);
+                    continue;
+                },
+            };
+        }
+        return directory;
+    } else {
+        kerror!("Couldn't reinitialize FAT Fs");
+        return fat_reinit();
+    }
 }
