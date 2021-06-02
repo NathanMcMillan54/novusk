8248250dd8611e472995b714f1ec504c09e221d8
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-06-02 12:27:07 -0400

    Worked on C stdlib.

--- lib/libc/src/stdio/mod.rs
+++ lib/libc/src/stdio/mod.rs
@@ -1,10 +1,20 @@
 use ctypes::{c_char, c_int};
+use novusk_syscalls::NovuskSysCalls;
+use core::any::Any;
 
 mod stdio;
 
 #[no_mangle]
 pub unsafe extern "C" fn printf(format: *const c_char, ...) -> c_int {
     let mut fmt = format;
-    stdio::vsprintf();
+    stdio::vsprintf(fmt);
     return 0;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn scanf(format: *const c_char, ...) -> c_char {
+    let mut sys = NovuskSysCalls;
+    let scan = sys.sys_input().read_key();
+    // TODO: Find C equivalant of Input
+    return "".as_ptr() as i8;
 }
--- lib/libc/src/stdio/stdio.rs
+++ lib/libc/src/stdio/stdio.rs
@@ -1,5 +1,8 @@
+use core::fmt::{Arguments, Write};
 use ctypes::{c_char, c_int};
+use libefi::st;
 
-pub unsafe fn vsprintf() {
-
+pub unsafe fn vsprintf(fmt: *const c_char) {
+    let stdout = st().as_ref().stdout();
+    writeln!(stdout, "{:?}", fmt);
 }
