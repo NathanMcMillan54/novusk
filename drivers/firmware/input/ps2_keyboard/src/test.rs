e0784d04959af5ec0358b108fcda5a09cacc92f5
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-06-07 17:39:47 -0400

    Added keyboard initializtion (PS2).

--- drivers/firmware/input/ps2_keyboard/src/test.rs
+++ drivers/firmware/input/ps2_keyboard/src/test.rs
@@ -2,7 +2,8 @@ use pc_keyboard::{HandleControl, ScancodeSet1, layouts};
 
 pub unsafe fn test_keyboard() {
     let mut kb = pc_keyboard::Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore);
-    // 0x20 = D
+    // This will "simulate" the D key being pressed
+    // Byte: 0x20 = D
     match kb.add_byte(0x20) {
         Ok(Some(event)) => {
             printk!("Event {:?}", event);
