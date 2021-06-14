a1445ee8a0d88d2d2151fcbac17707def79bb0b4
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-06-13 20:54:26 -0400

    Added Aarch64 support.

---
+++ arch/aarch64/src/kernel/init.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub extern "C" fn aarch64_kernel_init() -> ! {
+    loop {  }
+}
--- drivers/nkuefi/src/boot.rs
+++ drivers/nkuefi/src/boot.rs
@@ -1,14 +1,15 @@
 use core::fmt::Write;
+use crate::kernel;
 use uefi::{Handle, ResultExt};
 use uefi::table::{Boot, SystemTable};
 
 #[no_mangle]
-pub extern "C" fn efi_main(image: Handle, st: SystemTable<Boot>) -> ! {
+pub unsafe extern "C" fn efi_main(image: Handle, st: SystemTable<Boot>) -> ! {
     uefi_services::init(&st);
 
     st.stdout().reset(false).expect_success("Couldn't reset display");
 
     writeln!(st.stdout(), "Starting kernel...");
 
-    loop {  }
+    kernel::start_novusk()
 }
