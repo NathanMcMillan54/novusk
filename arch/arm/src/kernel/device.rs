50758b8de2a57e8ff67c7f564ad11badd2e11130
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-07-10 12:57:21 -0400

    Made nrf feature in arm more specific, (added exact board name).

--- arch/arm/src/kernel/device.rs
+++ arch/arm/src/kernel/device.rs
@@ -1,11 +1,24 @@
 use crate::nrf::nrf_init;
 
+pub static mut DEVICE: Board = Board::None;
+
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Board {
     Nrf52840,
     Nrf52832,
+    None,
 }
 
-pub unsafe fn device_init() {
+
+pub(crate) unsafe fn device_init() {
+    // Set device value
+    #[cfg(feature = "nrf52840")]
+    DEVICE = Board::Nrf52840;
+
+    #[cfg(feature = "nrf52832")]
+    DEVICE = Board::Nrf52832;
+
+    // Initialize device
     #[cfg(feature = "nrf")]
     nrf_init();
 }
