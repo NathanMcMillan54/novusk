392642fa0e7a8fd1c150cda5dca2b2b3ba123cb0
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2022-01-09 03:27:24 -0500

    Added module type and macro.

--- include/novusk/src/module.rs
+++ include/novusk/src/module.rs
@@ -33,3 +33,25 @@ macro_rules! start_module {
     };
 }
 
+// These will likely be used in the future. It will make module defining and calling easier.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum ModuleType {
+    Device,
+    DeviceIrq,
+    RunDeviceIrqs,
+    StorageDevice,
+    FileSystem,
+    GraphicsDriver,
+    Other
+}
+
+#[macro_export]
+macro_rules! v4_module_init {
+    ($module_type:pat, $km_name_init:ident, $km_name_start:ident) => {
+        #[export_name = stringify!([< $module_type _init >])]
+        #[no_mangle]
+        pub extern "C" fn $km_name_init() {
+            unsafe { $km_name_start(); }
+        }
+    };
+}
