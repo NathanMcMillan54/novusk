9a8ef3fa46905577e08706de110f6747313e6592
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2022-01-16 17:45:48 -0500

    Worked more on NKUEFI.

---
+++ drivers/boot/nkuefi/src/arch/x64.rs
@@ -0,0 +1,47 @@
+use alloc::string::ToString;
+use core::fmt::{Arguments, Write};
+use crate::{KERNEL_BIN_PATH, SfsInterface, _efi_print};
+use uefi::prelude::BootServices;
+use uefi::proto::media::file::{File, FileAttribute, FileInfo, FileMode, FileSystemVolumeLabel, RegularFile};
+use uefi::ResultExt;
+use uefi::table::boot::{AllocateType, MemoryType};
+use x86_64::VirtAddr;
+use xmas_elf::ElfFile;
+
+unsafe fn load_kernel(bt: &BootServices) -> (ElfFile, VirtAddr) {
+    let mut info_buffer = [0u8; 0x100];
+
+    let sfsi = SfsInterface::new();
+    let sfs = sfsi.get_sfs(bt);
+
+    let mut root = sfs.open_volume().unwrap().unwrap();
+    let volume_label = root.get_info::<FileSystemVolumeLabel>(&mut info_buffer).unwrap().unwrap().volume_label().to_string();
+
+    let kernel_handle = root.open(KERNEL_BIN_PATH, FileMode::Read, FileAttribute::empty()).expect_success("Couldn't kernel binary");
+    let mut kernel_handle = RegularFile::new(kernel_handle);
+
+    let info_mem = kernel_handle.get_info::<FileInfo>(&mut info_buffer).unwrap().unwrap();
+    _efi_print(format_args!("{}{}", "Kernel file name: ", info_mem.file_name().to_string()));
+
+    let pages = info_mem.file_size() / 0x1000 + 1;
+    let memory_start = bt.allocate_pages(AllocateType::AnyPages, MemoryType::LOADER_DATA, pages as usize).expect_success("Couldn't to alloc pages");
+
+    let buffer = core::slice::from_raw_parts_mut(memory_start as *mut u8, (pages * 0x1000) as usize);
+    let len: usize = kernel_handle.read(buffer).expect_success("Couldn't read kernel file");
+    let kernel_elf = ElfFile::new(buffer[..len].as_ref()).expect("Failed to parse kernel ELF");
+    let kernel_entry_addr = kernel_elf.header.pt2.entry_point();
+
+    _efi_print(format_args!("{}{}", "/boot/efi/kernel.elf Entry address: ", kernel_entry_addr));
+
+    return (kernel_elf, VirtAddr::new(kernel_entry_addr));
+}
+
+pub fn start_loading_kernel(bt: &BootServices) {
+    let (elf, kernel_addr) = unsafe { load_kernel(bt) };
+
+    _efi_print(format_args!("{}", "Starting kernel..."));
+
+    let kernel_entry: extern "sysv64" fn() = unsafe { core::mem::transmute(kernel_addr) };
+
+    kernel_entry();
+}
--- drivers/boot/nkuefi/src/main.rs
+++ drivers/boot/nkuefi/src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
 #![feature(asm)]
+#![allow(warnings)]
 
+#[macro_use] extern crate alloc;
 extern crate rlibc;
 #[macro_use] extern crate uefi_services;
 
@@ -9,9 +11,14 @@ use core::fmt::{Arguments, Write};
 use uefi::prelude::{Boot, Handle, SystemTable};
 use uefi_services::system_table;
 
+pub mod arch;
 pub mod gop;
+pub mod sfs;
 
 use gop::GopWriter;
+use crate::sfs::SfsInterface;
+
+pub const KERNEL_BIN_PATH: &'static str = "/boot/efi/kernel.elf";
 
 pub(crate) fn _efi_print(fmt: Arguments) {
     unsafe { writeln!(*system_table().as_mut().stdout(), "{}", fmt); }
@@ -28,7 +35,7 @@ pub extern "C" fn efi_main(image: Handle, mut system_table: SystemTable<Boot>) -
     _efi_print(format_args!("{}", "NKUEFI successfully booted\n"));
 
     // Delay for a bit
-    for _ in 0..9000000 { unsafe { asm!("nop"); } }
+    for _ in 0..99000000 { unsafe { asm!("nop"); } }
 
     let gop = GopWriter::new();
     gop.init(system_table.boot_services());
@@ -36,6 +43,18 @@ pub extern "C" fn efi_main(image: Handle, mut system_table: SystemTable<Boot>) -
     gop.clear_screen(system_table.boot_services(), (222, 165, 132));
 
     _efi_print(format_args!("{}", "GOP initialized"));
+
+    let sfs = SfsInterface::new();
+    let mut root = sfs.get_sfs(system_table.boot_services()).open_volume().unwrap().unwrap();
+
+    if root.read_entry(&mut vec![0; 128]).is_err() {
+        panic!("Failed to read root directory");
+    }
+
+    _efi_print(format_args!("{}", "Read root directory from SFS"));
+    _efi_print(format_args!("{}", "Loading kernel..."));
+
+    arch::start_loading_kernel(system_table.boot_services());
 
     panic!("Bootloader ended");
 }
