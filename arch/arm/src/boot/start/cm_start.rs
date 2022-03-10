bf7536b724dcf10afe0ded764e4773c193c316de
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2022-03-10 06:46:38 -0500

    ARM32 (Specifically Cortex M) kernel boots and clears linker memory.

--- arch/arm/src/boot/start/cm_start.rs
+++ arch/arm/src/boot/start/cm_start.rs
@@ -1,12 +1,21 @@
+use setup::BootSetup;
+use crate::boot::setup::ArmBoot;
+
+extern "C" {
+    fn boot_die() -> !;
+}
+
 #[link_section = ".vector_table.reset_vector"]
 #[no_mangle]
 pub static __RESET_VECTOR: unsafe extern "C" fn() -> ! = _start;
 
-#[path = "boot_h.rs"]
-mod boot_h;
-
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
+    let boot = ArmBoot::new();
 
-    boot_h::boot_die();
+    if boot.linker_setup().0.is_err() {
+        panic!("Failed to clear BSS");
+    }
+
+    boot_die();
 }
