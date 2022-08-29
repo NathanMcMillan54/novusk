a2bd39e137c1243ac585c52858b6ef4407333bbd
CATboardBETA <catboardbeta@gmail.com>
2022-08-29 10:44:29 -0400

    add fast and least types, still requires better impelementation

--- lib/cinclude/bits/stdint_intn/src/lib.rs
+++ lib/cinclude/bits/stdint_intn/src/lib.rs
@@ -3,5 +3,31 @@ pub type rint16_t = libc::c_short;
 pub type rint32_t = libc::c_int;
 pub type rint64_t = libc::c_longlong;
 
+// TODO: Come up with a better implementation for least types
+pub type rint_least8_t = libc::c_schar;
+pub type rint_least16_t = libc::c_short;
+pub type rint_least32_t = libc::c_int;
+pub type rint_least64_t = libc::c_longlong;
+
+// TODO: Come up with a better implementation for fast types
+pub type rint_fast8_t = libc::c_schar;
+pub type rint_fast16_t = libc::c_short;
+pub type rint_fast32_t = libc::c_int;
+pub type rint_fast64_t = libc::c_longlong;
+
 #[no_mangle]
-pub extern "C" fn __use_types_stdint_intn_h(_: rint8_t, _: rint16_t, _: rint32_t, _: rint64_t) {}
+pub extern "C" fn _use_types_stdint_intn_h(
+    _: rint8_t,
+    _: rint16_t,
+    _: rint32_t,
+    _: rint64_t,
+    _: rint_fast8_t,
+    _: rint_fast16_t,
+    _: rint_fast32_t,
+    _: rint_fast64_t,
+    _: rint_least8_t,
+    _: rint_least16_t,
+    _: rint_least32_t,
+    _: rint_least64_t,
+) {
+}
--- lib/cinclude/bits/stdint_uintn/src/lib.rs
+++ lib/cinclude/bits/stdint_uintn/src/lib.rs
@@ -3,11 +3,31 @@ pub type ruint16_t = libc::c_ushort;
 pub type ruint32_t = libc::c_uint;
 pub type ruint64_t = libc::c_ulonglong;
 
+// TODO: Come up with a better implementation for least types
+pub type ruint_least8_t = libc::c_uchar;
+pub type ruint_least16_t = libc::c_ushort;
+pub type ruint_least32_t = libc::c_uint;
+pub type ruint_least64_t = libc::c_ulonglong;
+
+// TODO: Come up with a better implementation for fast types
+pub type ruint_fast8_t = libc::c_uchar;
+pub type ruint_fast16_t = libc::c_ushort;
+pub type ruint_fast32_t = libc::c_uint;
+pub type ruint_fast64_t = libc::c_ulonglong;
+
 #[no_mangle]
 pub extern "C" fn __use_types_stdint_uintn_h(
     _: ruint8_t,
     _: ruint16_t,
     _: ruint32_t,
     _: ruint64_t,
+    _: ruint_fast8_t,
+    _: ruint_fast16_t,
+    _: ruint_fast32_t,
+    _: ruint_fast64_t,
+    _: ruint_least8_t,
+    _: ruint_least16_t,
+    _: ruint_least32_t,
+    _: ruint_least64_t,
 ) {
 }
